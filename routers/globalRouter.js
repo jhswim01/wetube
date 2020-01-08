279a9d2568cd9f83ad79cf98a1c732e42ed188a5
heeyeong <jhswim01@gmail.com>
2020-01-08 21:37:56 +0900

    webpack 4.0

---
+++ routers/globalRouter.js
@@ -0,0 +1,25 @@
+import express from "express";
+import routes from "../routes";
+import { home, search } from "../controller/videoController";
+import {
+  getJoin,
+  login,
+  logout,
+  postJoin,
+  getLogin,
+  postLogin
+} from "../controller/userController";
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.search, search);
+
+export default globalRouter;
---
+++ routers/videoRouter.js
@@ -0,0 +1,32 @@
+import express from "express";
+import routes from "../routes";
+import {
+  video,
+  getUpload,
+  postUpload,
+  videoDetail,
+  getEditVideo,
+  postEditVideo,
+  deleteVideo
+} from "../controller/videoController";
+import { uploadVideo } from "../middlewares";
+
+const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, video);
+
+//upload ideo
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+//video detail
+videoRouter.get(routes.videoDetail(), videoDetail);
+
+//edit video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//delete video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
+
+export default videoRouter;

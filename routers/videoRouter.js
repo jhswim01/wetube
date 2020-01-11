43002c76ae0f6c30ac52972cfed7f42caac253ca
heeyeong <jhswim01@gmail.com>
2020-01-11 22:10:36 +0900

    exports error fixed

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -15,18 +15,18 @@ const videoRouter = express.Router();
 
 videoRouter.get(routes.videos, video);
 
-//upload ideo
+// upload ideo
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
-//video detail
+// video detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
-//edit video
+// edit video
 videoRouter.get(routes.editVideo(), getEditVideo);
 videoRouter.post(routes.editVideo(), postEditVideo);
 
-//delete video
+// delete video
 videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
